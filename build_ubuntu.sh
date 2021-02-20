cdf1d6005c337d3bdfdbce6fbcbe735ee8a87c4a
willzhang <willzhmic@outlook.com>
2021-02-20 18:30:15 +0800

    v2.9

--- build_ubuntu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-BOOKSTACK_VERSION=v2.9
-docker build -t willdockerhub/bookstack:ubuntu_${BOOKSTACK_VERSION} -f Dockerfile.ubuntu .
--- run_bookstack.sh
+++ run_bookstack.sh
@@ -2,5 +2,5 @@
 docker run -d --name bookstack \
   --restart always \
   -p 8181:8181 \
-  -v /data/bookstack/conf:/opt/bookstack/conf \
-  willdockerhub/bookstack
+  -v /data/bookstack/conf:/bookstack/conf \
+  willdockerhub/bookstack:v2.9_node
