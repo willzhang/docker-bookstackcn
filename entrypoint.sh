214f5f0f4c99fcca979e19b78ed59ab02aea87ef
will <willzhmic@outlook.com>
2021-02-24 22:24:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-if [ ! -f /bookstack/dbupdated ]
-then
-  /bookstack/BookStack install 2>&1 | tee /bookstack/dbupdated
-else
-  echo "###db had updated,skip bookstack install,just start it!###"
-fi
-/bookstack/BookStack
+#!/bin/sh
+/bookstack/BookStack install && /bookstack/BookStack
